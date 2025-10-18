59eef4d996d42281559323c16d2ab2e8614ba13f
Jacob Reimer <jacobr5229@gmail.com>
2025-10-17 22:16:38 -0700

    Inital Commit

---
+++ Models/ShotAnalysis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+public enum ShotType { Unknown = 0, JumpShot = 1, SetShot = 2 }
+
+public enum Handedness { Unknown = 0, Right = 1, Left = 2 }
+
+public enum FootStance {Unknown = 0, RightForward = 1, LeftForward = 2, Square = 3, Other = 4}
+
+
+
+
+namespace FixMyShot.Models
+{
+    public class ShotAnalysis
+    {
+
+
+    }
+}
